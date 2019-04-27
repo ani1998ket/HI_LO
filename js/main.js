5776c476dfefd1f3c228f01838c28ac3b659ab25
Aniket Kumar <ani1998ket@gmail.com>
2019-04-27 23:04:48 +0530

    first-iteration

---
+++ js/main.js
@@ -0,0 +1,64 @@
+var diceImagesRed = [
+                    "res/images/dieRed1.png",
+                    "res/images/dieRed2.png",
+                    "res/images/dieRed3.png",
+                    "res/images/dieRed4.png",
+                    "res/images/dieRed5.png",
+                    "res/images/dieRed6.png"
+                ];
+
+let diceValue = 1;
+let score = 0;
+let animationLoop;
+let frame = 0;
+let angle = 0;
+let isgameOver = false;
+
+reset();
+function buttonPress(buttonPress){
+    // diceValue = ((diceValue%6)+6)%6;
+    btn_high.disabled = true;
+    btn_low.disabled = true;
+    let oldDiceValue = diceValue;
+    diceValue = 1 + Math.round(Math.random() * 5);
+    if(diceValue >= oldDiceValue && buttonPress){
+        score++;
+    }else
+    if(diceValue <= oldDiceValue && !buttonPress){
+        score++;
+    }else{
+        isgameOver = true;
+    }
+    animationLoop = window.setInterval(rollDice, 30); 
+}
+
+function gameOver(){
+    alert("you scored " + score + " points");
+    reset();
+}
+function reset(){
+    score = 0;
+    score_box.innerHTML = score;    
+    isgameOver = false;
+}
+function rollDice(){
+    
+    frame++;
+    angle = 10 * Math.sin(frame);
+    if(frame > 50) {
+        frame = 0;
+        angle = 0;
+        clearInterval(animationLoop);
+        dice_image.src = diceImagesRed[diceValue - 1];
+        console.log(score);
+        if(isgameOver){
+            window.setTimeout(gameOver, 500);
+        }
+        score_box.innerHTML = score;
+        btn_high.disabled = false;
+        btn_low.disabled = false;
+    }
+    dice_image.style.transform = "rotate("+angle+"deg)";
+    
+    
+}
